5f779804bfc96f6752cf9654995dba567c711973
yuanhecai <yuanhecai@loongson.cn>
2023-10-11 17:25:28 +0800

    Delete all files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/loongson/shequ/yhc_shequ/SDL
-# Build directory: /home/loongson/shequ/yhc_shequ/SDL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
